f1b8c074a64b80d0831920cfa9682d4dc2453261
Eric5603 <ericwanyoike123@gmail.com>
2025-01-17 22:23:31 +0300

    Add the Navigation Bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
-
+import React from 'react';
+import Nav from './components/Nav';
 export default function Home() {
   return (
-   <div>
-    <h1 className="text-base text-gray-900 justify-center items-center flex">Welcome to My Website</h1>
-   </div>
+        <>
+            <Nav/>
+        </>
+  
   );
 }
