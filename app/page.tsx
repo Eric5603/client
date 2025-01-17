438c80bc999b08bd1a6716f957c578ee97ed1c6e
Eric5603 <ericwanyoike123@gmail.com>
2025-01-17 13:44:53 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <h1 className="text-base text-gray-900 justify-center items-center flex">Welcome to My Website</h1>
+   </div>
+  );
+}
